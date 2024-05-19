02234c6d6d78953d723c1b5717ff520dc6708920
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2024-05-19 14:37:58 -0500

    Add files via upload

---
+++ ns.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void answer(char question[]);
+
+int main(){
+        char question[100];
+
+        printf("Ask me a question: ");
+        scanf("%s", question);
+        if (strcmp(question, "") != 0){
+            answer(question);
+        }
+        else{
+            printf("Please ask a question\n");
+        }
+
+    return 0;
+}
+
+void answer(char question[]){
+    char answer[100];
+    FILE *file = fopen("answer.txt", "r");
+    if (file == NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
+    //each line is in ascii format of 105 100 107 
+    //print line one after another and convert to letter
+    while (fscanf(file, "%s", answer) != EOF){
+        for (int i = 0; i < strlen(answer); i++){
+            printf("%c", answer[i]);
+        }
+        printf("\n");
+    }
+    fclose(file);
+}
